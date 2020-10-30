e79b502d19c3f6d862624079bcfdb664b458963d
Piotr Szpara-Soliński <72048074+PiotrSzpara@users.noreply.github.com>
2020-10-30 22:34:29 +0100

    1st game commit

---
+++ src/test/java/com/kodillajavagame/kodillagame/KodillaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodillajavagame.kodillagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
